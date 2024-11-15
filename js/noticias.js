5db6e5298f4fea7250675d8cab9250d8cc002e93
Ernesto Ardenghi <eardenghi@gmail.com>
2024-11-15 17:25:56 -0300

    correcciones menores

--- js/noticias.js
+++ js/noticias.js
@@ -4,6 +4,13 @@ axios.get(url)
 .then(response => {
     let noticias = response.data.data.noticias;
     console.log(noticias);
+    for (let i = 0; i < 4; i++) {
+        let card = document.getElementById(i);
+        let h3 = card.querySelector("h3");
+        let p = card.querySelector("p");
+
+        h3.textContent = "prueba";
+    }
     /*
     let card = 
     <div class="card">
