4ced6b7e413a5308cfcdc89df1a8d22db9f63551
Ernesto Ardenghi <eardenghi@gmail.com>
2024-11-15 17:30:54 -0300

    correcciones menores

--- js/noticias.js
+++ js/noticias.js
@@ -8,9 +8,11 @@ axios.get(url)
         let card = document.getElementById(i);
         let h3 = card.querySelector("h3");
         let p = card.querySelector("p");
+        let img = card.querySelector("img");
 
         h3.textContent = noticias[i].titulo;
         p.textContent = noticias[i].subtitulo;
+        img.src = noticias[i].cartelera;
     }
 })
 .catch(error => {
