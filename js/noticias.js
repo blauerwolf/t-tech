983aba7458c4fca5c530b8c1de81ca13b314533e
Ernesto Ardenghi <eardenghi@gmail.com>
2024-11-15 17:28:05 -0300

    correcciones menores

--- js/noticias.js
+++ js/noticias.js
@@ -9,19 +9,9 @@ axios.get(url)
         let h3 = card.querySelector("h3");
         let p = card.querySelector("p");
 
-        h3.textContent = "prueba";
+        h3.textContent = noticias[i].titulo;
+        p.textContent = noticias[i].subtitulo;
     }
-    /*
-    let card = 
-    <div class="card">
-                    <img src="https://via.placeholder.com/300" alt="Imagen de la tarjeta" class="card-image">
-                    <div class="card-content">
-                        <h3 class="card-title">Título de la Tarjeta</h3>
-                        <p class="card-description">Esta es una descripción breve de la tarjeta. Aquí puedes poner más detalles si lo deseas.</p>
-                        <button class="card-button">Leer más</button>
-                    </div>
-                </div>
-    */
 })
 .catch(error => {
     console.error('Error:', error); // Maneja errores
