32c809b0f3bb2f07af34e58e183fec899e80f035
Ernesto Ardenghi <eardenghi@gmail.com>
2024-11-15 16:53:48 -0300

    correcciones menores

--- js/noticias.js
+++ js/noticias.js
@@ -1,4 +1,9 @@
-fetch('https://api.provinciaradio.com.ar/v1/noticias')
+fetch('https://api.provinciaradio.com.ar/v1/noticias', {
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+})
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
