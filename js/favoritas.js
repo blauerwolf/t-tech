b63e2c2b7d8de24c53b32f21f3513e1bbc7035bf
Ernesto Ardenghi <eardenghi@gmail.com>
2024-12-11 12:29:18 -0300

    Maquetado de pagina de favoritos... in progress

--- js/favoritas.js
+++ js/favoritas.js
@@ -13,16 +13,74 @@ async function getNoticiaById(noticiaId) {
 function crearDetalleNoticia(data) {
     console.log(data);
 
-    const contenedor = document.createElement('div');
-    contenedor.className = 'card';
+    const row = document.createElement('div');
+    row.className = 'row row-noticia border rounded p-2 shadow';
 
-    const imgContainer = document.createElement('div');
+    const colLeft = document.createElement('div');
+    colLeft.className = 'col d-flex flex-column justify-content-between';
+
     const img = document.createElement('img');
+    img.className = 'img-thumbnail';
     img.src = data.cartelera;
     img.alt = 'Imagen de la noticia';
 
+    const button = document.createElement('button');
+    button.className = 'btn btn-danger';
     
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-trash';
+    
+    const colRight = document.createElement('div');
+    colRight.className = 'col-9';
 
+    const titulo = document.createElement('h2');
+    titulo.className = 'noticia-titulo';
+    titulo.textContent = data.titulo;
+
+    const autor = document.createElement('p')
+    autor.className = 'noticia-autor';
+    autor.textContent = data.autor;
+
+    const volanta = document.createElement('h4');
+    volanta.className = 'noticia-volanta';
+    volanta.textContent = data.volanta;
+
+    const texto = document.createElement('div');
+    texto.className = 'noticia-texto';
+    texto.textContent = data.texto;
+
+    button.appendChild(icon);
+
+    colLeft.appendChild(img);
+    colLeft.appendChild(button);
+
+    colRight.appendChild(titulo);
+    colRight.appendChild(autor);
+    colRight.appendChild(volanta);
+    colRight.appendChild(texto);
+
+    row.appendChild(colLeft);
+    row.appendChild(colRight);
+
+    // Agregar la tarjeta al contenedor "noticias-container"
+    const noticiasContainer = document.getElementById('listado-favoritas');
+    if (noticiasContainer) {
+        noticiasContainer.appendChild(row);
+    } else {
+        console.error('El contenedor con el ID "noticias-container" no existe.');
+    }
+}
+
+function borrarNoticia(noticiaId) {
+    let favoritosIds = JSON.parse(localStorage.getItem('noticiasFavoritas')) || [];
+
+    if (favoritosIds.includes(noticiaId)) {
+        favoritosIds = favoritosIds.filter((id) => id !== cardId);
+    }
+
+    localStorage.setItem('noticiasFavoritas', JSON.stringify(favoritosIds));
+
+    // TODO: Borrar el div con la noticia
 }
 
 function cargarDetalleFavoritos() {
