f25d642e16ef0327823a3079fa43ed9938e25b84
Ernesto Ardenghi <eardenghi@gmail.com>
2025-10-31 15:42:31 -0300

    fix: Correcciones para WCAG en iframes

--- js/noticias.js
+++ js/noticias.js
@@ -8,7 +8,7 @@ function crearCard(cardId, imageUrl, title, description) {
 
     const img = document.createElement('img');
     img.src = imageUrl;
-    img.alt = `Imagen de la noticia: ${title}. ${descripcion}`;
+    img.alt = `Imagen de la noticia: ${title}. ${description}`;
     img.className = 'card-img-top card-img-fixed';
 
     const cardBody = document.createElement('div');
